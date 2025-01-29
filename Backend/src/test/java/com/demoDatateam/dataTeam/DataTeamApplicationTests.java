2111d052d10529425a7eef1505b1ea5f4fa5571a
MancipeManuel <162451732+MancipeManuel@users.noreply.github.com>
2025-01-29 10:19:20 -0500

    completar carpeta bakend
    agregar spring

---
+++ Backend/src/test/java/com/demoDatateam/dataTeam/DataTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoDatateam.dataTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
